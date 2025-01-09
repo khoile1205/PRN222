2d4aca27935aa86b6e377819611ed57b1d49093c
Le Dinh Khoi <dinhkhoi.le3@gmail.com>
2025-01-09 14:17:16 +0700

    initialize base code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Caffeine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
